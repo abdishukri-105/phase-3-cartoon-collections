fd94bcb7500b74cad79a13b9c4f7707ecb8565db
abdishukri <abdishukrimohamed105@gmail.com>
2023-02-08 11:12:19 +0300

    passed all tests

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,36 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require "pry"
+
+def roll_call_dwarves (names)
+  names.each_with_index do |name, index|
+    puts "#{index + 1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+
+def summon_captain_planet (planeteer_calls)
+  planeteer_calls.map do |calls|
+     calls.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+# used any method here which is used to check if 
+# any elem in array satisfies the condition
+def long_planeteer_calls(calls)
+  calls.any? { |call| call.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+
+
+# ingredients is our arg which should be array 
+# and i used find method to to iterate through
+# using include method array i then checked to see if
+# the ingredients contained any of my cheeses 
+def find_the_cheese(ingredients)
+  cheeses = ["cheddar", "gouda", "camembert"]
+  ingredients.find { |ingredient| cheeses.include?(ingredient) }
 end
+
+
+
+binding.pry
